9ba02cbdcb4a4c32b5797e294250393fcbf16fbc
Jeremy Gann <56943728+Jgann123@users.noreply.github.com>
2021-03-30 14:46:08 -0400

    added some styling and loading spinner

--- src/SeasonDisplay.js
+++ src/SeasonDisplay.js
@@ -1,6 +1,19 @@
 import React from "react";
+import './SeasonDisplay.css'
 
-const getSeasons = (lat, month) => {
+const seasonConfig = {
+    summer: {
+        text: 'Lets hit the beach!',
+        iconName: 'sun'
+    },
+    winter: {
+        text: 'Burrr it is cold!',
+        //just a reminder this "snowflake... is the name of the icon in the semantics-ui. " same for the summer object. 
+        iconName: 'snowflake'
+    }
+};
+
+const getSeason = (lat, month) => {
   if (month > 2 && month < 9) {
     return lat > 0 ? "summer" : "winter";
   } else {
@@ -9,11 +22,16 @@ const getSeasons = (lat, month) => {
 };
 
 const SeasonDisplay = (props) => {
-  const seasons = getSeasons(props.lat, new Date().getMonth());
-  
+  const seasons = getSeason(props.lat, new Date().getMonth());
+  const { text, iconName } = seasonConfig[seasons] 
 
-  return <div>{seasons === 'winter' ? 'Burr, it is chilly.' : 'Lets hit the beach!'}</div>
-
+  return (
+    <div className={`season-display ${seasons}`}>
+    <i className={`icon-left massive ${iconName} icon`} />
+     <h1> {text}</h1>
+     <i className ={`icon-right massive ${iconName} icon`} />
+    </div>
+  );
 };
 
 export default SeasonDisplay;
